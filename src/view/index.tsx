e6fcef2675e3e4b510389d73c4db0e7f852d50d4
tuphan-dn <tuphan@descartes.network>
2022-08-05 14:57:35 +0700

    update senhub

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
-import EmbededView from '@sentre/embeded-view'
 import { useUI, net } from '@sentre/senhub'
 
-import configs from 'configs'
+import EmbededView from '@sentre/embeded-view'
 import MainnetOnly from './mainnetOnly'
+
+import configs from 'configs'
 
 const {
   manifest: { appId },
